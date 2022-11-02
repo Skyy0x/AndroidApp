67730badad3da8b17935c2c37c6e7f4345607354
_Sky0x <tymek.ski@gmail.com>
2022-11-02 16:10:20 +0100

    Adding method descriptions

--- app/src/main/java/pl/sky0x/app/data/DataSystem.java
+++ app/src/main/java/pl/sky0x/app/data/DataSystem.java
@@ -4,7 +4,16 @@ import java.util.Collection;
 
 public interface DataSystem {
 
+    /**
+     * This method is used to retrieve clicks
+     * @return Clicks from database/api
+     */
     Collection<Click> getClicks();
 
+    /**
+     * This method is used to adding click to database/api
+     * @param click that sending
+     * @return the operation was successful
+     */
     boolean addClick(Click click);
 }
